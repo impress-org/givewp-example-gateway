dc97cba20f83176949679a1fbbb5d8ed88186b95
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2023-11-02 13:08:54 -0400

    docs: update with subscription

--- class-onsite-example-gateway-subscription-module.php
+++ class-onsite-example-gateway-subscription-module.php
@@ -29,10 +29,10 @@ class ExampleGatewayOnsiteSubscriptionModuleClass extends SubscriptionModule
                 throw new PaymentGatewayException(__('Example payment ID is required.', 'example-give'));
             }
 
-            // Step 2: Create a payment with your gateway.
+            // Step 2: Create a subscription with your gateway.
             $response = ExampleGatewayApi::createSubscription(['transaction_id' => $gatewayData['example-gateway-id']]);
 
-            // Step 3: Return a command to complete the donation. You can alternatively return PaymentProcessing for gateways that require a webhook or similar to confirm that the payment is complete. PaymentProcessing will trigger a Payment Processing email notification, configurable in the settings.
+            // Step 3: Return a command to complete the subscription. You can alternatively return SubscriptionProcessing for gateways that require a webhook or similar to confirm that the subscription is complete. SubscriptionProcessing will trigger an email notification, configurable in the settings.
             return new SubscriptionComplete($response['transaction_id'], $response['id']);
         } catch (Exception $e) {
             // Step 4: If an error occurs, you can update the donation status to something appropriate like failed, and finally throw the PaymentGatewayException for the framework to catch the message.
